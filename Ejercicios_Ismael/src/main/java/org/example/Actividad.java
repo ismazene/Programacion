e555c890dfb5b4e40c3fefc3d93f5148d5a90d2e
A603\1DAW <ismaelzeneddine@gmail.com>
2025-09-25 17:10:54 +0200

    ++

---
+++ Ejercicios_Ismael/src/main/java/org/example/Actividad.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Actividad {
+    static void main (){
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("¿Como te llamas?");
+        String nombre = teclado.nextLine();
+
+        System.out.println("¿¿A que curso vas??");
+        String curso = teclado.next();
+
+        System.out.println("Hola " + nombre + " bienvenido a " + curso);
+
+        System.out.println("Introduce un numero..");
+        int numero1 = teclado.nextInt();
+
+        System.out.println("Introduce un numero..");
+        int numero2 = teclado.nextInt();
+
+        int resultado = numero1 + numero2;
+        System.out.println("El resultado es " + resultado);
+
+    }
+}
