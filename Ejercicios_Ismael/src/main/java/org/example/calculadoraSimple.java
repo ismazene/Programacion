f0eb34cfb2b95c319fcaea27a070a586f0f68a98
A603\1DAW <ismaelzeneddine@gmail.com>
2025-10-24 15:40:26 +0200

    --

---
+++ Ejercicios_Ismael/src/main/java/org/example/calculadoraSimple.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class calculadoraSimple {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("***** BIENVENIDO A LA CALCULADORA RÁPIDA *****");
+
+
+ahdagfafsdgfa
+
+
+
+    }
+}
