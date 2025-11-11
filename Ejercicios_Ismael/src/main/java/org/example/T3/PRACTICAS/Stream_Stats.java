43654931c25788a461dc9b5495aac6dbb63daace
ismazene <ismaelzeneddine@gmail.com>
2025-11-11 18:51:06 +0100

    Correccion
    Removed unnecessary loop for parsing espectadores.

--- Ejercicios_Ismael/src/main/java/org/example/T3/PRACTICAS/Stream_Stats.java
+++ Ejercicios_Ismael/src/main/java/org/example/T3/PRACTICAS/Stream_Stats.java
@@ -1,5 +1,6 @@
 package org.example.T3.PRACTICAS;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Stream_Stats {
@@ -22,10 +23,7 @@ public class Stream_Stats {
         boolean correcto;
         String[] espectadores_String;
         int[] espectadores = new int[5];
-
-        for (int i = 0; i < vector_espectadores.length; i++) {
-            int espectadores_int = Integer.parseInt(Arrays.toString(vector_espectadores));
-
+        
         do {
             correcto = true;
             System.out.print("Inserta los espectadores medidos: ");
